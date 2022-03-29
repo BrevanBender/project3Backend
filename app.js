c168648ae52615155443ba41bc4d9352934fc1e5
BrevanBender <bendercoding@gmail.com>
2022-03-29 11:22:32 -0700

    still trying for heroku

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
-require("dotenv").config()
-const { urlencoded } = require('express')
-const express = require('express')
-const cors = require('cors')
-const mongoose = require("mongoose")
-const morgan = require("morgan")
-const app = express()
-app.use(morgan('short'))
-app.use(cors())
-app.use(urlencoded({extended: true}))
-app.use(express.json())
+const express = require("express");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const Post = require("./models/post");
+const MongoDBStore = require("connect-mongodb-session")(session);
+require("dotenv").config();
+const app = express();
 
-const mongoURI = process.env.MONGO_URI
-mongoose.connect(mongoURI)
-mongoose.connection.once('open', ()=>{
-    console.log('connected to mongod...');
+//Connection middleware for express
+const cors = require("cors");
+//Morgan is a logger
+const morgan = require("morgan");
+
+app.use(cors());
+//Shorter than default, also including response time.
+app.use(morgan("short"));
+
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "mySessions",
 });
-const Post = require("./models/post")
-const postController = require('./controllers/postController')
-const User = require("./models/user")
 
+require("./db-utils/connect");
 
-app.use('/', postController)
-app.listen(3001, ()=>{
+const postController = require("./controllers/postController");
 
-})
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use("/posts", postController);
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log("App Running: " + port);
+});
